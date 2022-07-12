24d97ade2b157169c685633c070d3b63260c6d68
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2022-07-12 14:59:57 +0530

    Day 1 and 2

--- Input3.java
+++ Input3.java
@@ -3,7 +3,12 @@ import java.io.Console;
 public class Input3 {
     public static void main(String[] args) {
         Console c = System.console();
+        System.out.print("Enter your username: ");
+        String uname = c.readLine();
+        System.out.println("Enter your password: ");
         char[] ch = c.readPassword();
-        System.out.println(ch.toString());
+        System.out.println("Username: " + uname);
+        System.out.print("Password: ");
+        System.out.println(ch);
     }
 }
