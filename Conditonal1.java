e7f947d41875758cf2ca44cfa8f3540a86d72ba4
zohaib166 <92329661+zohaib166@users.noreply.github.com>
2022-07-12 15:33:12 +0530

    Day 1 and 2

---
+++ Conditonal1.java
@@ -0,0 +1,9 @@
+public class Conditonal1 {
+    public static void main(String[] args) {
+        //int x = 1;
+        int y = 3;
+        if((y & 1)==1) {
+            System.out.println("hi");
+        } 
+    }
+}
--- Input4.java
+++ Input4.java
@@ -1,6 +1,11 @@
+//run on cmd prompt using javac and java separately
 public class Input4 {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+        if (args.length < 1) {
+            System.out.println("Please supply atleast one number");
+        } else {
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+        }
     }
 }
